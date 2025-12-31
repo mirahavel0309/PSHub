07550368d2c536e27c1d2f5761e929ea3babdf28
MiraHavel <bagjinhyeong640@gmail.com>
2025-12-31 21:42:42 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+
+	ll n, r;
+	cin >> n >> r;
+	cout << r * 2 + n - 1;
+}
