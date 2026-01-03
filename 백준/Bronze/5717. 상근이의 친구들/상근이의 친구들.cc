88720fafe297f8d0196dcfd02250c2fcaaf74449
MiraHavel <bagjinhyeong640@gmail.com>
2026-01-03 19:37:21 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2084 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+
+	int m, f;
+	while(cin >> m >> f) {
+		if (m == 0 && f == 0) break;
+		cout << m + f << '\n';
+	}
+
+}
