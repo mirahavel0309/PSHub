1f6e005829d06ce6b15944881975b055ab616e97
MiraHavel <bagjinhyeong640@gmail.com>
2026-01-03 19:32:27 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2088 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+
+	string a, b;
+	cin >> a >> b;
+	cout << (a.size() >= b.size() ? "go" : "no");
+}
